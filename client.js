6becbe047902a7e8ef4af952b483160e9dc4fe40
JoshGrant5 <joshgg@icloud.com>
2020-09-23 19:26:43 +0000

    Uncommented idle message

--- client.js
+++ client.js
@@ -4,8 +4,8 @@ const net = require('net');
  */
 const connect = () => {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541 
+    host: '135.23.222.131',
+    port: 50542 
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
