7a85d9df7e3ac7e39ef9c2e74024d2856d04a8bb
JoshGrant5 <joshgg@icloud.com>
2020-09-23 18:44:19 +0000

    Sends server a message, naming the snake

--- client.js
+++ client.js
@@ -10,9 +10,19 @@ const connect = () => {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+   // message sent to the server once connected
+   conn.on('connect', () => { 
+    console.log('Successfully connected to game server');
+  });
+
+   // send the server a message, naming the snake
+  conn.on('connect', () => { 
+    conn.write('Name: JGG');
+  });
+
   // message to client after server kicks them out for idling too long
   conn.on('data', (data) => { 
-    console.log('you ded cuz you idled')
+    console.log(data); // 'you ded cuz you idled'
   });
 
   return conn;
